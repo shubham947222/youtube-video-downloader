7dc13cf978cf948c30ff99e8d2767bc265375287
k.shubham <shubham947222@gmail.com>
2024-01-16 23:54:04 +0530

    error hadling

--- app/page.jsx
+++ app/page.jsx
@@ -9,9 +9,18 @@ export default function Home() {
   const [finalLink, setFinalLink] = useState("");
   const [showDownload, setShowDownload] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const handleDownload = () => {
-    setLoading(true);
+    if (videoLink?.length === 0) {
+      setError(true);
+      setTimeout(() => {
+        setError(false);
+      }, 2000);
+    } else {
+      setError(false);
+      setLoading(true);
+    }
     axios
       .get(`/api/downloader?url=${videoLink}`)
       .then((res) => {
@@ -43,8 +52,29 @@ export default function Home() {
           <p></p>
         </div>
       </header>
-
-      <div className="flex text-white bg-indigo-300 rounded-md flex-col items-center min-h-[450px] w-full justify-center">
+      {error && (
+        <div class="bg-indigo-900 text-center py-4 lg:px-4">
+          <div
+            class="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex"
+            role="alert"
+          >
+            <span class="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">
+              Please
+            </span>
+            <span class="font-semibold mr-2 text-left flex-auto">
+              Paste the correct link
+            </span>
+            {/* <svg
+              class="fill-current opacity-75 h-4 w-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
+            </svg> */}
+          </div>
+        </div>
+      )}
+      <div className="flex text-white bg-indigo-400 rounded-md flex-col items-center min-h-[450px] w-full justify-center">
         <h3>
           {loading ? "Thank you for waiting" : "Shubh Youtube Video Downloader"}
         </h3>
@@ -61,7 +91,7 @@ export default function Home() {
             />
             <button
               onClick={handleDownload}
-              className="rounded-md py-1 px-4 font-semibold border transition duration-300 hover:bg-white hover:text-indigo-300"
+              className="rounded-md py-1 px-4 font-semibold border transition duration-300 hover:bg-white hover:text-indigo-400"
             >
               view
             </button>
